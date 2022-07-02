2ef1277b815417c0157fb9fb222885f31a55f81c
YOUR_NAME <YOUR_EMAIL>
2022-07-02 20:41:28 +0300

    Add files

--- nowdatetime.py
+++
@@ -1,6 +0,0 @@
-import datetime as dt
-import locale
-
-locale.setlocale(locale.LC_ALL, "ru")  # задаем локаль для вывода даты на русском языке
-time_now = dt.datetime.now().strftime("%H")
-dt_now = dt.datetime.now().strftime("%a: %d.%m.%y")
