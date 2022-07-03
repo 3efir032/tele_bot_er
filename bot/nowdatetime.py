c4952285d32c00397de11917dbf7d0bfc81ddc29
3efir032 <105213833+3efir032@users.noreply.github.com>
2022-07-03 13:34:27 +0300

    Update nowdatetime.py

--- bot/nowdatetime.py
+++ bot/nowdatetime.py
@@ -1,6 +1,6 @@
 import datetime as dt
-import locale
+#import locale
 
-locale.setlocale(locale.LC_ALL, "ru")  # задаем локаль для вывода даты на русском языке
+#locale.setlocale(locale.LC_ALL, "ru")  # задаем локаль для вывода даты на русском языке
 time_now = dt.datetime.now().strftime("%H")
 dt_now = dt.datetime.now().strftime("%a: %d.%m.%y")
