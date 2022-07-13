2f59609cdbc19179314f60f62467894f3f06c70b
YOUR_NAME <YOUR_EMAIL>
2022-07-13 20:39:48 +0300

    Add files

--- bot/coin_parsing.py
+++ bot/coin_parsing.py
@@ -1,42 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-"""ОПИСАНИЕ"""
+'''ОПИСАНИЕ - парсит курс крипты с investing.com
+Код работает медленно
+'''
 
-class Coin:
+
+def url_wedside(link_coin):
+    link = f'https://ru.investing.com/crypto/{link_coin}'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
 
-    def __init__(self, coin_name='bitcoin', numcoin='1057391'):
-        self.url = f'https://ru.investing.com/crypto/{coin_name}'
-        self.numcoin = numcoin
-        self.name = coin_name
-        self.price = self.get_btc()
+    try:
+        full_page_coin = requests.get(link, headers=headers)
+        soup = BeautifulSoup(full_page_coin.content, 'lxml')
+        return soup
+    except requests.exceptions.ConnectionError:
+        return '443'
 
-    def get_btc(self):
-        full_page_btc = requests.get(self.url, headers=self.headers)
-        soup = BeautifulSoup(full_page_btc.content, 'lxml')
-        price = soup.find("div", class_="fullHeaderTwoColumnPage--top cryptoTopColumn").find("div", class_="inlineblock").find("div", class_="top").find("span", class_="inlineblock").find("span", class_="pid-1057391-last")
-        return price.text
-    #def __str__(self):
-        #return f"{self.price}"
 
-def write(data, filename):  # Запись файла JSON
+def parsing_coin(soup, idcoin):
+    price_coin = soup.find("div", class_="fullHeaderTwoColumnPage--top cryptoTopColumn").find("span",
+                                                                                              class_="inlineblock").find(
+        "span", class_=f"pid-{idcoin}-last").text
+    return price_coin
+
+
+def start(namecoin, idcoin):
+    url = url_wedside(namecoin)
+    if url != '443':
+        return parsing_coin(url, idcoin)
+    else:
+        return 'error'
+
+
+def coinprice():
+    bitcoin = start('bitcoin', '1057391')
+    ethereum = start('ethereum', '1061443')
+    tether = start('tether', '1061453')
+    binance_usd = start('binance-usd', '1177192')
+    bnb = start('bnb', '1061448')
+    return bitcoin, ethereum, tether, binance_usd, bnb
+
+def write(data, filename): # Запись файла JSON
     data = json.dumps(data)
     data = json.loads(str(data))
     with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=2)
+        json.dump(data, file, indent = 2)
 
-def write_json():
+def write1_json():
     data = {
-        "COIN": [],
+        "FUTURESNOW": [],
     }
-    data["COIN"].append(Coin().__dict__)
+    data["FUTURESNOW"].append(coinprice())
     write(data, 'price_coin.json')
 
-def main():
-    write_json()
 
 
 if __name__ == '__main__':
-    main()
+    write1_json()
+
+# BTC 1057391
+# ETH 1061443
+# BUSD 1177192
+# USDT 1061453
+# BNB 1061448
